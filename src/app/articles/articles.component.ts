5ec23d6ca08a00ea97bc31f1798db96ad8dba587
Dieyna22 <101632839+Dieyna22@users.noreply.github.com>
2023-11-22 15:19:37 +0000

    commit 5

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -15,9 +15,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class ArticlesComponent {
   recupArticle: any;
-
   articleId:any;
   AjoutArticle: any;
+
+  inputTitre: string = '';
+  inputBody: string = '';
+
 
   constructor(private http : HttpClient,private articlesService: ArticleServiceService, private ajoutArticle: AjoutArticleServiceService,private deleteService:DeleteServiceService) { }
 
@@ -30,10 +33,6 @@ export class ArticlesComponent {
     this.articlesService.getArticles().subscribe((articles: any) => {
       this.recupArticle = articles;
     })
-
-    console.log(this.envoyerRequete());
-
-
   }
 
  voirPlus(article: any) {
@@ -44,39 +43,66 @@ export class ArticlesComponent {
   deleteArticle(articleId:any){
     console.log(articleId)
   }
-  userId: any;
+
   userName: any;
   userEmail: any;
 
-  MAJUsers() {
-    const urlUser = `https://jsonplaceholder.typicode.com/users/${this.userId}`;
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-
-    const users = {
-      name: this.userName,
-      email: this.userEmail
-    };
-  }
-
-  envoyerRequete() {
-    const url = 'https://jsonplaceholder.typicode.com/posts';
-    const postData = {
-      title: 'titre',
-      body: 'lorem ipsum',
+  MAJUsers(paramArticle: any) {
+    alert(paramArticle);
+    const urlArticle = `https://jsonplaceholder.typicode.com/users/${paramArticle}`;
+    const putData = {
+      id: 1,
+      title: 'foo',
+      body: 'bar',
       userId: 1,
-    };
+    }
 
-    this.http.post(url, postData)
+    this.http.post(urlArticle, putData)
       .subscribe((response) => {
         console.log(response);
         return response;
       });
+    
+  }
+
+  // envoyerRequete() {
+  //   const url = 'https://jsonplaceholder.typicode.com/posts';
+  //   const postData = {
+  //     title: this.inputTitre,
+  //     body: this.inputBody,
+  //     userId: 1,
+  //   };
+  //   this.inputTitre = '',
+  //   this.inputBody = '',
+
+  //   this.http.post(url, postData)
+  //     .subscribe((response) => {
+  //       console.log(response);
+  //       return response;
+  //     });
+  // }
+
+  articleFound() {
+    this.itemSearch = this.recupArticle.filter(
+      (item: any) => (item?.title.toLowerCase().includes(this.searchArticle.toLowerCase())));
+  }
+
+  posts: any[] = [];
+  nouvelArticle = { title: this.inputTitre, body: this.inputBody};
+
+  ajouterArticle() {
+    const titreTemporaire = this.nouvelArticle.title;
+    const contenuTemporaire = this.nouvelArticle.body;
+
+    this.ajoutArticle.PostArticle(this.nouvelArticle).subscribe((response: any) => {
+      console.log('Réponse du service après ajout d\'article :', response);
+      this.posts.push(response); // Ajouter le nouvel article à la liste existante
+   
+
+      this.nouvelArticle = { title: '', body: '' };
+    });
+
+    // Afficher les valeurs pour le débogage
+    console.log('Valeurs après ajout :', titreTemporaire, contenuTemporaire);
   }
 }
-
-
- 
-  
- 
- 
-
